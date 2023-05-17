19a4eca2a11c90531172be4f557c8707f380038e
GenAnderson <genchung1@outlook.com>
2023-05-16 14:17:38 -1000

    exercise 2.9 midway

--- index.js
+++ index.js
@@ -13,26 +13,36 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let auth = require("./auth")(app);
+const passport = require("passport");
+require("./passport");
+
+// allows mongoose to connect to db to perform CRUD operations
 mongoose.connect("mongodb://localhost:27017/test", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// welcome page response to user
 app.get("/", (req, res) => {
   res.send("Welcome to my movies app! There's still a lot of work to be done!");
 });
 
 // get all movies
-app.get("/movies", (req, res) => {
-  Movies.find()
-    .then((movie) => {
-      res.status(201).json(movie);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error:" + err);
-    });
-});
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Movies.find()
+      .then((movie) => {
+        res.status(201).json(movie);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error:" + err);
+      });
+  }
+);
 
 // get all users
 app.get("/users", (req, res) => {
@@ -112,6 +122,17 @@ app.post("/users", (req, res) => {
     .catch((error) => {
       console.error(error);
       res.status(500).send("Error:" + error);
+    });
+});
+
+app.get("/users", function (req, res) {
+  Users.find()
+    .then(function (users) {
+      res.status(201).json(users);
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.status(500).send("Error: " + err);
     });
 });
 
