0fe6c8d1e0a7616b547e676a843e66be72dd0e06
GenAnderson <genchung1@outlook.com>
2023-05-30 13:09:57 -1000

    testing: changed mongoose.connect info to see if deployment works

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ require("./passport.js");
 const Movies = Models.Movie;
 const Users = Models.User;
 // allows mongoose to connect to db to perform CRUD operations
-mongoose.connect("mongodb://localhost:27017/test", {
+mongoose.connect("mongodb://127.0.0.1:27017/test", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
