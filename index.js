1e34523fa5075bef7d29833c09ad7c5bebff93d8
GenAnderson <genchung1@outlook.com>
2023-06-26 13:29:40 -1000

    changed cors restriction

--- index.js
+++ index.js
@@ -15,22 +15,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // allows CORs
 const cors = require("cors");
-app.use(cors());
+
+// allows all to access (not recommended)
+// app.use(cors());
 
 // restrict origin using CORS
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://testsite.com",
+  "https://bestmovielist.netlify.app",
+];
 
-// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message ), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 let auth = require("./auth.js")(app);
 const passport = require("passport");
